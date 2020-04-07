636f7e83514f8efab7e9c8fc603074ef7f529f25
John Higgins <jph91@cam.ac.uk>
2020-04-07 17:57:00 +0100

    Created the introductory methods and constructor for the Server class. Also some small error handling and future TODOs.

--- src/Server.java
+++ src/Server.java
@@ -1,24 +1,40 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.net.*;
+import java.io.*;
 
 public class Server {
-    private List<Thread> serverThreads;
+    private HashSet<ServerThread> serverThreads;
+    private ServerSocket serverSocket;
+    private int port;
 
-    public static void main(String[] args) {
-        Server server = new Server(10);
+    public static void main(String[] args) throws IOException {
+        Server server = new Server(Integer.parseInt(args[0]));
         server.run();
     }
 
-    private Server(int initial_thread_count) {
-        serverThreads = new ArrayList<>(initial_thread_count);
-        for (int i = 0; i < initial_thread_count; i++) {
-            serverThreads.add(new Thread(new ServerThread()));
+    private Server(int port) {this.port = port;}
+
+    private void run() throws IOException {
+        while (true){
+            try {
+                serverSocket = new ServerSocket(port);
+                serverSocket.setReuseAddress(true);
+                serverThreads.add(new ServerThread(serverSocket.accept()));
+
+            } catch (IOException e) {
+                //TODO: Decide whether the error is worth letting run up to the main funciton for catching there
+                //TODO: Log Fatal Error
+                //This quits the system should it encounter an error when creating a port for the server
+                System.out.println("FATAL ERROR");
+                System.exit(1);
+            }
         }
     }
 
-    private void run() {
-        for (Thread thread: serverThreads) {
-            thread.start();
-        }
+    public void closeThread(ServerThread c){
+        serverThreads.remove(c);
+        //TODO: Log Closure
     }
 }
--- src/ServerThread.java
+++ src/ServerThread.java
@@ -3,6 +3,9 @@ import java.io.*;
 
 public class ServerThread implements Runnable {
 
+    public ServerThread(Socket accept) {
+    }
+
     @Override
     public void run() {
         //TODO: handle connections
