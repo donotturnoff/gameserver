2dc9543981128de785f56a1c528f176a93b4654b
donotturnoff <admin@donotturnoff.net>
2020-04-07 17:19:55 +0100

    Created Server constructor containing simple thread initialisation routine and a run method to run each thread

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,24 @@
-public class Server {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Server {
+    private List<Thread> serverThreads;
+
+    public static void main(String[] args) {
+        Server server = new Server(10);
+        server.run();
+    }
+
+    private Server(int initial_thread_count) {
+        serverThreads = new ArrayList<>(initial_thread_count);
+        for (int i = 0; i < initial_thread_count; i++) {
+            serverThreads.add(new Thread(new ServerThread()));
+        }
+    }
+
+    private void run() {
+        for (Thread thread: serverThreads) {
+            thread.start();
+        }
+    }
 }
